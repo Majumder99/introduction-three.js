3b70a4607969a777447b4d7ff28252f2b5772ef8
ACER <souravmajumderk99@gmail.com>
2023-09-18 00:28:55 +0600

    adding threejs

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { easing } from "maath";
+import { useFrame } from "@react-three/fiber";
+import { useSnapshot } from "valtio";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
+
+import state from "../store";
+
+const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+  return (
+    <>
+      <group>
+        <mesh
+          castShadow
+          geometry={nodes.T_Shirt_male.geometry}
+          material={materials.lamber1}
+          material-roughness={1}
+          dispose={null}></mesh>
+      </group>
+    </>
+  );
+};
+
+export default Shirt;
